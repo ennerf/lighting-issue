6263eb20d467170ee7ef992c3d2465cc608f584b
Florian Enner <florian@hebirobotics.com>
2023-10-20 17:21:11 +0200

    added windows images

--- src/main/java/us/hebi/samples/Lighting3d.java
+++ src/main/java/us/hebi/samples/Lighting3d.java
@@ -1,17 +1,11 @@
 package us.hebi.samples;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.SceneAntialiasing;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Material;
-import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
 import javafx.scene.shape.Sphere;
 import javafx.stage.Stage;
@@ -20,9 +14,11 @@ import javafx.stage.Stage;
 public class Lighting3d extends Application {
 
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        System.out.println("Running JavaFX " + javaVersion + ", on Java " + javafxVersion);
+        stage.setTitle(String.format("%s + Java %s + JavaFX %s",
+                System.getProperty("os.name"),
+                System.getProperty("java.version"),
+                System.getProperty("javafx.version")
+        ));
 
         var root = new Group();
         int n = 200;
